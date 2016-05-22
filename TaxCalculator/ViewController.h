d126af6ea22d53f4645b91e9d585eebeeff09287
Christopher Benavides <realchrisb@icloud.com>
2016-05-22 17:41:39 -0500

    Initial Commit

---
+++ TaxCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaxCalculator
+//
+//  Created by Christopher Benavides on 5/22/16.
+//  Copyright © 2016 Christopher Benavides. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
